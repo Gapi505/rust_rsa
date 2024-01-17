f41dff21a769e663326bff0a7ba63abb8d44e824
Gapi505 <81484469+Gapi505@users.noreply.github.com>
2024-01-17 19:55:56 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -7,136 +7,16 @@ use num_bigint::{BigUint,BigInt, ToBigUint, RandBigInt, ToBigInt};
 use num_traits::{One, Zero};
 use num_integer::lcm;
 use std::collections::HashMap;
-use dialoguer::{theme::ColorfulTheme, Input, FuzzySelect, Confirm};
-use std::fs::File;
-use std::io::prelude::*;
-use serde_json::json;
 
 
 fn main() {
-    app();
-}
-fn app(){
-
-    //gets both keys from json files
-    let mut public_file = File::open("public_key.json").unwrap_or_else(|_| {
-        let (public, _) = create_key_pair(10); // replace 10 with the number of prime digits you want
-        save_key_pair(&public, &HashMap::new()).unwrap(); // replace HashMap::new() with your private key if you have it
-        File::open("public_key.json").unwrap()
-    });
-    let mut public_json = String::new();
-    public_file.read_to_string(&mut public_json).unwrap();
-    let public: HashMap<String, BigUint> = convert_hashmap_sb(&serde_json::from_str(&public_json).unwrap());
-
-    let mut private_file = File::open("private_key.json").unwrap_or_else(|_| {
-        let (_, private) = create_key_pair(10); // replace 10 with the number of prime digits you want
-        save_key_pair(&HashMap::new(), &private).unwrap(); // replace HashMap::new() with your public key if you have it
-        File::open("private_key.json").unwrap()
-    });
-    let mut private_json = String::new();
-    private_file.read_to_string(&mut private_json).unwrap();
-    let private: HashMap<String, BigUint> = convert_hashmap_sb(&serde_json::from_str(&private_json).unwrap());
-
-    let mut passwords_file = File::open("passwords.json").unwrap_or_else(|_| {
-        let passwords: HashMap<String, String> = HashMap::new(); // replace with your passwords if you have them
-        let passwords_json = serde_json::to_string(&passwords).unwrap();
-        let mut passwords_file = File::create("passwords.json").unwrap();
-        passwords_file.write_all(passwords_json.as_bytes()).unwrap();
-        File::open("passwords.json").unwrap()
-    });
-    let mut passwords_json = String::new();
-    passwords_file.read_to_string(&mut passwords_json).unwrap();
-    let mut passwords: HashMap<String, String> = serde_json::from_str(&passwords_json).unwrap();
-
-    let actions = &[
-        "Generate new key pair",
-        "Save password",
-        "Get password",
-    ];
-    let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
-        .with_prompt("What do you want to do?")
-        .default(0)
-        .items(&actions[..])
-        .interact()
-        .unwrap();
-    match selection {
-        0 => {
-            let prime_digits = Input::<u32>::new()
-                .with_prompt("How many digits should the prime numbers have?")
-                .interact()
-                .unwrap();
-            let (public, private) = create_key_pair(prime_digits);
-            println!("Public key: {:?}", public);
-            println!("Private key: {:?}", private);
-            save_key_pair(&public, &private).unwrap();
-
-            // resets passwords
-            passwords = HashMap::new();
-            save_passwords(&passwords).unwrap();
-        },
-        1 => {
-            let name = Input::<String>::new()
-                .with_prompt("What is the name of the password?")
-                .interact()
-                .unwrap();
-            let password = Input::<String>::new()
-                .with_prompt("What is the password?")
-                .interact()
-                .unwrap();
-            passwords.insert(name, encrypt(password, public["e"].clone(), public["n"].clone()));
-            save_passwords(&passwords).unwrap();
-        },
-        2 => {
-            let password_names: Vec<String> = passwords.keys().map(|s| s.to_string()).collect();
-            let name = FuzzySelect::with_theme(&ColorfulTheme::default())
-                .with_prompt("Which password do you want to get?")
-                .default(0)
-                .items(&password_names[..])
-                .interact()
-                .unwrap();
-            let name = password_names[name].clone();
-            let password = decrypt(passwords[&name].clone(), private["d"].clone(), private["n"].clone());
-            println!("Password: {:?}", password);
-        },
-        _ =>{
-            println!("Not implemented yet")
-        }
-    }
-    if Confirm::new()
-        .with_prompt("Do you want to do something else?")
-        .interact()
-        .unwrap()
-    {
-        app();
-    }
-}
-
-fn save_key_pair(public: &HashMap<String, BigUint>, private: &HashMap<String, BigUint>) -> std::io::Result<()> {
-    let public_json = json!(convert_hasmap_bs(public));
-    let private_json = json!(convert_hasmap_bs(private));
-
-    let mut public_file = File::create("public_key.json")?;
-    public_file.write_all(public_json.to_string().as_bytes())?;
-
-    let mut private_file = File::create("private_key.json")?;
-    private_file.write_all(private_json.to_string().as_bytes())?;
-
-    Ok(())
-}
-fn convert_hasmap_bs(hashmap: &HashMap<String, BigUint>) -> HashMap<String, String> {
-    hashmap.iter().map(|(k, v)| (k.clone(), v.to_string())).collect()
-}
-fn convert_hashmap_sb(hashmap: &HashMap<String, String>) -> HashMap<String, BigUint> {
-    hashmap.iter().map(|(k, v)| (k.clone(), BigUint::from_str(v).unwrap())).collect()
-}
-
-fn save_passwords(passwords: &HashMap<String, String>) -> std::io::Result<()> {
-    let passwords_json = json!(passwords);
-
-    let mut passwords_file = File::create("passwords.json")?;
-    passwords_file.write_all(passwords_json.to_string().as_bytes())?;
-
-    Ok(())
+    let message = "Hello World!".to_string();
+    let (public, private) = create_key_pair(6);
+    let encrypted_message = encrypt(message.clone(), public.get("e").unwrap().clone(), public.get("n").unwrap().clone());
+    let decrypted_message = decrypt(encrypted_message.clone(), private.get("d").unwrap().clone(), private.get("n").unwrap().clone());
+    println!("Message: {}", message);
+    println!("Encrypted Message: {}", encrypted_message);
+    println!("Decrypted Message: {}", decrypted_message);
 }
 
 fn generate_prime(digits: u32) -> BigUint {
